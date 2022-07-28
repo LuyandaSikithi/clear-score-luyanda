aa747e361b674f26a78a984dcdf35a028d34daa4
Luyanda Sikithi <l.sikithi@outlook.com>
2022-07-28 15:52:24 +0200

    Initial Commit

---
+++ ClearScore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClearScore
+//
+//  Created by Luyanda Sikithi on 2022/07/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
